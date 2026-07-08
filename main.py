a2056386801d48d8d9da6398c00d76afe1763883
Dakota Nagy <dnagy6@student.cscc.edu>
2026-07-08 10:54:31 -0400

    Imported primary classes into main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Program: Hybrid Training & Pace Calculator
+Author: Dakota Nagy
+Purpose: This main.py is a terminal utility to calculate barbell percentages
+         and running pacing splits.
+"""
+
+from data.storage import DataStorage
+from calculators.lifting import LiftingCalculator
+from calculators.pacing import PacingCalculator
+
